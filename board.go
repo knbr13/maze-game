31eaab5286329d1af694c66c84b239b2e2d12595
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-10 23:52:05 +0300

    update print func

--- board.go
+++ board.go
@@ -18,10 +18,14 @@ type board struct {
 	playerYPos int
 }
 
+const (
+	PLAYER = "┃"
+)
+
 func initializeCells(width, height int) cells {
-	cells := make(cells, height)
+	cells := make(cells, height*2+1)
 	for i := range cells {
-		cells[i] = make([]cell, width)
+		cells[i] = make([]cell, width*2+1)
 		for j := range cells[i] {
 			cells[i][j].isWall = true
 		}
@@ -30,46 +34,29 @@ func initializeCells(width, height int) cells {
 }
 
 func (c *cells) carve(x, y int) {
-	(*c)[y][x].isWall = true
+	(*c)[y][x].visited = true
 	directions := rand.Perm(4)
 
 	for _, direction := range directions {
 		switch direction {
 		case 0:
-			if y-2 >= 0 && !(*c)[y-2][x].isWall {
-				(*c)[y-1][x].visited = true
+			if y-2 > 0 && !(*c)[y][x].visited {
 				(*c)[y-1][x].isWall = false
 				c.carve(x, y-2)
-			}
-		case 1:
-			if x+2 < len((*c)[0]) && !(*c)[y][x+2].isWall {
-				(*c)[y][x+1].visited = true
-				(*c)[y][x+1].isWall = true
-				c.carve(x+2, y)
-			}
-		case 2:
-			if y+2 < len(*c) && !(*c)[y+2][x].isWall {
-				(*c)[y+1][x].visited = true
-				(*c)[y+1][x].isWall = true
-				c.carve(x, y+2)
-			}
-		case 3:
-			if x-2 >= 0 && !(*c)[y][x-2].isWall {
-				(*c)[y][x-1].visited = true
-				(*c)[y][x-1].isWall = false
-				c.carve(x-2, y)
 			}
 		}
 	}
 }
 
-func (c *cells) print() {
-	for i := range *c {
-		for j := range (*c)[i] {
-			if (*c)[i][j].isWall {
-				fmt.Print(" ")
-			} else {
+func (b *board) print() {
+	for i, row := range b.cells {
+		for j, col := range row {
+			if i == b.playerYPos && j == b.playerXPos {
+				fmt.Print(PLAYER)
+			} else if col.isWall {
 				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
 			}
 		}
 		fmt.Println()

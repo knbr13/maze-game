8d3200331a21c81201ec623cc5f9469142412157
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-11 01:04:42 +0300

    start the game

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	difficultyLevel := getDifficultyLevel()
 	board := initializeBoard(difficultyLevel)
 	board.cells.carve(board.playerXPos, board.playerYPos)
-	// clearConsole()
-	// initialTime := 60
-	board.print()
+	clearConsole()
+	initialTime := 60
+	fmt.Println(board)
 
+	reader := bufio.NewReader(os.Stdin)
+	runeReceiverChan := make(chan rune)
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		go func() {
+			r, _, err := reader.ReadRune()
+			if err == nil {
+				runeReceiverChan <- r
+			}
+		}()
+
+		select {
+		case <-ticker.C:
+			initialTime--
+			clearConsole()
+			fmt.Printf("%v\nremaining: %v\n", board, initialTime)
+		case r := <-runeReceiverChan:
+			board.handleMove(r)
+			clearConsole()
+			fmt.Printf("%v\nremaining: %v\n", board, initialTime)
+		}
+	}
 }
 
 func getDifficultyLevel() string {
@@ -44,7 +70,7 @@ func initializeBoard(difficultyLevel string) *board {
 		width, height := 24, 10
 		return &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
 	case "easy":
-		width, height := 2, 2
+		width, height := 12, 6
 		return &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
 	}
 	return &board{}

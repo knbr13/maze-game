8a09d02870072565077ac1d2710d11b6d55b723d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-10 17:30:37 +0300

    add initializeCells function

--- board.go
+++ board.go
@@ -10,3 +10,11 @@ type board struct {
 	playerXPos int
 	playerYPos int
 }
+
+func initializeCells(width, height int) [][]cell {
+	cells := make([][]cell, height)
+	for i := range cells {
+		cells[i] = make([]cell, width)
+	}
+	return cells
+}

2ea10d907a31136e813ed27596e8f45cc56b76d2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-11 09:43:32 +0300

    update initializeBoard

--- main.go
+++ main.go
@@ -76,16 +76,23 @@ func getDifficultyLevel() string {
 }
 
 func initializeBoard(difficultyLevel string) *board {
+	var b *board
 	switch difficultyLevel {
 	case "hard":
 		width, height := 32, 16
-		return &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
+		b = &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
 	case "medium":
 		width, height := 24, 10
-		return &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
+		b = &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
 	case "easy":
 		width, height := 12, 6
-		return &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
+		b = &board{cells: initializeCells(width, height), playerXPos: (rand.Intn(width) * 2) + 1, playerYPos: (rand.Intn(height) * 2) + 1}
 	}
-	return &board{}
+	b.cells.carve(b.playerXPos, b.playerYPos)
+	gateX, gateY := findFarthestPoint(b.cells, b.playerXPos, b.playerYPos)
+	b.gateXPos = gateX
+	b.gateYPos = gateY
+	b.cells[gateY][gateX].isWall = false
+	return b
+
 }
